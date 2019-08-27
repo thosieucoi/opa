49fcfb08f10f87dd011b116716ddff090e64e489
Tho Nguyen <thocoder@gmail.com>
2019-08-27 14:29:40 +0900

    update config

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { Nuxt, Builder } = require('nuxt');
 const app = express();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.BASE_URL || 'http://chat.trustscore.tech:3000';
 
 app.set('port', port);
 
